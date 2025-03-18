d312d36d0453d56eea1eb313518764e35bfcef57
Oladipupo Bilikis Arike <oladipupobaliki13@gmail.com>
2025-03-18 03:33:27 +0100

    Second Commit

---
+++ component/src/Components/UserCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserCard = ({name, email, bio}) => {
+  return (
+    <div className="max-w-sm bg-white shadow-lg rounded-2xl p-5 text-center border">
+        <h2 className='text-xl font-semibold mt-3'> {name}</h2>
+        <p className='text-gray-500 text-sm'>{email}</p>
+        <p className='mt-2 text-gray-700'>{bio || "No bio Available"}</p>
+    </div>
+  )
+}
+
+export default UserCard
