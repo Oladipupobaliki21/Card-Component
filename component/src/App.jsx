b6b1c7e90d66b6d321648f88ee08f75d645e4c3d
teslim <tunjiheritage@gmail.com>
2025-03-18 19:00:32 +0100

    done

--- component/src/App.jsx
+++ component/src/App.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import UserCard from './Components/UserCard'
+import React from 'react';
+import UserCard from './Components/UserCard.jsx';
+
 const users = [
   {
-    name:"Oladipupo Bilikis Arike",
-    email: "oladipupobilikisarike@gamil.com",
+    name: "Oladipupo Bilikis Arike",
+    email: "oladipupobilikisarike@gmail.com",
     bio: "Front-End Developer",
   },
   {
-    name:"Olatunji Teslim",
-    email:"olatunji@gmail.com",
-    bio:"Full Stack Developer",
+    name: "Olatunji Teslim",
+    email: "olatunji@gmail.com",
+    bio: "Full Stack Developer",
   },
   {
-    name:"John Ayanyemi Abiodun",
-    email:"john@gmail.com",
-    bio:"Designer",
+    name: "John Ayanyemi Abiodun",
+    email: "john@gmail.com",
+    bio: "Designer",
   }
-]
+];
+
 const App = () => {
   return (
     <div className='flex flex-col items-center min-h-screen bg-gray-100 py-10'>
       <h1 className="text-3xl font-bold mb-5">User Profile Cards</h1>
-      <div className='flex flex-gap-8'>
-      {users.map( (user, index) => {
-        <UserCard key={index} {...user}/>
-      }
-      )}
+      <div className='flex gap-8'>
+        {users.map((user, index) => (
+          <UserCard key={index} {...user} />
+        ))}
       </div>
- 
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
